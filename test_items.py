5151e58d64904ab0a56f44cfb7ce255adef1bf4f
Rita <60928926+margaritashub@users.noreply.github.com>
2020-03-04 23:05:06 +0500

    time sleep deleted english error message added

--- test_items.py
+++ test_items.py
@@ -3,6 +3,6 @@ link ='http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_add_to_basket_button_is_present(browser):
     browser.get(link)
-    sleep(30)
+
     add_to_basket = browser.find_element_by_css_selector('.btn-add-to-basket')
-    assert add_to_basket, 'кнопка добавления товара не найдена'
+    assert add_to_basket, 'could not find the add button'
